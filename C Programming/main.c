6a0dd430b704ca82ccfaa367428255171678a959
zmartin96 <zachary1131@gmail.com>
2023-08-28 15:19:56 -0500

    Initial Commit

---
+++ C Programming/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C Programming
+//
+//  Created by Zachary Martin on 8/28/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
